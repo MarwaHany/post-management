c035d84ddde14fcb8986c4d9a3a957ea3f428576
MarwaHany <hanym9259@gmail.com>
2022-12-31 16:38:04 +0200

    - restrict view and change permissions.

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
 
-from .models import Comment, Post
+from .models import Comment, Post, User
+
+
+def check_perm(user_obj):
+    if user_obj.is_superuser or user_obj.is_staff:
+        return True
+    return False
+
+
+class PostAdmin(admin.ModelAdmin):
+    def check_perm(self, user_obj):
+        if user_obj.is_superuser or user_obj.is_staff:
+            return True
+        return False
+
+    def has_view_permission(self, request):
+        return check_perm(request.user)
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class CommentAdmin(admin.ModelAdmin):
+    def has_view_permission(self, request, obj=None):
+        return check_perm(request.user)
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
